fce6d97c43c9388d91fafdd8c0294d91e21e8e30
Dovydas Venckus <dovydas.venckus@live.com>
2020-12-02 22:39:51 +0200

    Query 24 hours data

--- weather-api/weather-api.py
+++ weather-api/weather-api.py
@@ -1,16 +1,23 @@
+from datetime import (datetime, timedelta)
 from flask import (Flask, jsonify)
 import sqlite3
 
 app = Flask(__name__)
 app.config["DEBUG"] = True
 
-WEATHER_QUERY = "SELECT * FROM weather"
+WEATHER_QUERY = """
+SELECT * FROM weather
+WHERE date > ? AND ? > date
+"""
 
 @app.route('/api/weather', methods=['GET'])
 def weather():
   DB_CONNECTION = sqlite3.connect('../weather-grabber/weather.db')
   cursor = DB_CONNECTION.cursor()
-  cursor.execute(WEATHER_QUERY)
+  now = datetime.now()
+  yesterday = now - timedelta(days=1)
+
+  cursor.execute(WEATHER_QUERY, (yesterday, now))
   data = cursor.fetchall()
   DB_CONNECTION.close()
   return jsonify(data)
